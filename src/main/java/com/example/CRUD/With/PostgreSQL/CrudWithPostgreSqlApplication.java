b51e841244119716914e29910574fc0a9cfb661f
Shakibul-Islam <shakibulislam0112@gmail.com>
2025-09-30 00:40:06 +0600

    CRUD with postgreSQL, Java, Spring Boot, JS

---
+++ src/main/java/com/example/CRUD/With/PostgreSQL/CrudWithPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD.With.PostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithPostgreSqlApplication.class, args);
+	}
+
+}
